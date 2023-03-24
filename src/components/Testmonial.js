a0bf2e562fcc15a7f02580f0ee00e04c4a874faf
Shubham Kr <72254047+shubhamkr83@users.noreply.github.com>
2023-03-24 09:32:07 +0530

    Add project to git

---
+++ src/components/Testmonial.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const Testmonial = () => {
+    return (
+        <div>
+            <div className="flex justify-center testmonial">
+                <div className="flex flex-col justify-center items-center max-w-7xl">
+                    <div className="text-3xl text-purple-700 font-bold mt-4 ">Testimonial</div>
+                    <div className="text-2xl text-black font-bold mt-2 ">What our Customers Say</div>
+                    <div className="flex flex-col items-center justify-evenly my-12 lg:px-10 lg:flex-row space-y-8 lg:space-y-0 ">
+                        <div
+                            className="flex flex-col justify-center items-center p-10 bg-white shadow w-[90%] md:w-[70%] lg:w-[30%] overflow-hidden">
+                            <div className="flex flex-col -mt-20 -mr-[352px] hidden xl:block">
+                                <div className="w-2 h-24 bg-sky-500"></div>
+                                <div className="w-2 h-14 bg-purple-700"></div>
+                            </div>
+                            <h1 className="text-xl tracking-tight font-semibold text-purple-700 uppercase   ">1. Best Components</h1>
+                            <p className="text-black mt-3 text-center w-[90%] lg:w-[100%]"> Tailblocks provides best Tailwind CSS
+                                Components and Blocks to create an unique websites within minutes. It has upto 60+ free
+                                components for front-end Web Development.</p>
+                            <img src="https://source.unsplash.com/150x150/?man" className="rounded-full mt-4" alt="" />
+                            <div className="flex flex-col -ml-[352px] mt-72 absolute hidden xl:block">
+                                <div className="w-2 h-24 bg-sky-500"></div>
+                                <div className="w-2 h-14 bg-purple-700"></div>
+                            </div>
+                            <h1 className="text-lg font-semibold text-purple-700 uppercase mt-1">mark warner</h1>
+                            <h1 className="text-lg font-semibold text-black uppercase -mt-1">ceo / director</h1>
+                        </div>
+                        <div
+                            className="flex flex-col justify-center items-center p-10 bg-white shadow w-[90%] md:w-[70%] lg:w-[30%] overflow-hidden">
+                            <div className="flex flex-col -mt-20 -mr-[352px] hidden xl:block">
+                                <div className="w-2 h-24 bg-sky-500"></div>
+                                <div className="w-2 h-14 bg-purple-700"></div>
+                            </div>
+                            <h1 className="text-xl tracking-tight font-semibold text-purple-700 uppercase   ">2. Amazing Designs</h1>
+                            <p className="text-black mt-3 text-center w-[90%] lg:w-[100%]"> Tailblocks provides best Tailwind CSS
+                                Components and Blocks to create an unique websites within minutes. It has upto 60+ free
+                                components for front-end Web Development.</p>
+                            <img src="https://source.unsplash.com/150x150/?girl" className="rounded-full mt-4" alt="" />
+                            <div className="flex flex-col -ml-[352px] mt-72 absolute hidden xl:block">
+                                <div className="w-2 h-14 bg-sky-500"></div>
+                                <div className="w-2 h-14 bg-purple-700"></div>
+                            </div>
+                            <h1 className="text-lg font-semibold text-purple-700 uppercase mt-1">alexa daniel</h1>
+                            <h1 className="text-lg font-semibold text-black uppercase -mt-1">ceo / director</h1>
+                        </div>
+                        <div
+                            className="flex flex-col justify-center items-center p-10 bg-white shadow w-[90%]  md:w-[70%] lg:w-[30%] overflow-hidden">
+                            <div className="flex flex-col -mt-20 -mr-[352px] hidden xl:block">
+                                <div className="w-2 h-24 bg-sky-500"></div>
+                                <div className="w-2 h-14 bg-purple-700"></div>
+                            </div>
+                            <h1 className="text-xl tracking-tight font-semibold text-purple-700 uppercase   ">3. Impressive</h1>
+                            <p className="text-black mt-3 text-center w-[90%] lg:w-[100%]"> Tailblocks provides best Tailwind CSS
+                                Components and Blocks to create an unique websites within minutes. It has upto 60+ free
+                                components for front-end Web Development.</p>
+                            <img src="https://source.unsplash.com/150x150/?woman" className="rounded-full mt-4" alt="" />
+                            <div className="flex flex-col -ml-[352px] mt-72 absolute hidden xl:block">
+                                <div className="w-2 h-14 bg-sky-500"></div>
+                                <div className="w-2 h-14 bg-purple-700"></div>
+                            </div>
+                            <h1 className="text-lg font-semibold text-purple-700 uppercase mt-1">jessy bush</h1>
+                            <h1 className="text-lg font-semibold text-black uppercase -mt-1">ceo / director</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Testmonial
